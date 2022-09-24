1533534991d981752d682f21306e8d4d4fdbc010
Abdallah Farag <abdallahfarag72@gmail.com>
2022-09-24 10:44:46 +0200

    remove unnecessary console.log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log('client side js file is running');
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
